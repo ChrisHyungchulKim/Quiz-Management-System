4e08783e30eb751b0cff2eb4c845871600b7714b
seannowak <94653040+seannowak@users.noreply.github.com>
2022-04-05 12:11:26 -0400

    created question class

---
+++ Question.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class Question {
+	private String prompt;
+	private ArrayList<String> responses;
+	private int answer;
+	private int weight;
+	
+	public Question(String prompt, ArrayList<String> responses, int answer, int weight) {
+		this.prompt = prompt;
+		this.responses = responses;
+		this.answer = answer;
+		this.weight = weight;
+	}
+
+	public String getPrompt() {
+		return prompt;
+	}
+
+	public ArrayList<String> getResponses() {
+		return responses;
+	}
+
+	public int getAnswer() {
+		return answer;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setPrompt(String prompt) {
+		this.prompt = prompt;
+	}
+
+	public void setResponses(ArrayList<String> responses) {
+		this.responses = responses;
+	}
+
+	public void setAnswer(int answer) {
+		this.answer = answer;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	// Maybe add equals method as well? To test if questions already exist?
+
+	public String toString() {
+		String format = "Question{prompt = %s, responses = [";
+		for (int i = 0; i < responses.size(); i++) {
+			format += responses.get(i) + ", ";
+		}
+		format = format.substring(0, format.length() - 2) + "], answer = %s, weight = %d}";
+		String answerString = responses.get(answer);
+		return String.format(format, this.prompt, answerString, this.weight);
+	}
+}
---
+++ Quiz.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Quiz {
+	private String name;
+	private ArrayList<Question> questions;
+
+	public Quiz(String name, ArrayList<Question> questions) {
+		this.name = name;
+		this.questions = questions;
+	}
+
+	public Quiz(String fileName) {
+		//read quiz details from file
+		//this.name =
+		//this.questions =
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ArrayList<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void addQuestion(Question question, int index) {
+		// if index is null, add question at end of list
+	}
+
+	public void removeQuestion(Question question) {
+
+	}
+
+	public void writeFile(String fileName, ArrayList<Question> questions) {
+
+	}
+
+	public int calcMaxScore(ArrayList<Question> questions) {
+		// Not sure if this method is necessary yet
+		return maxScore;
+	}
+
+	public int takeQuiz(boolean random) {
+		// Will probably handle console I/O from this method directly
+		// Will return score which can be written to specific course file?
+		return score;
+	}
+
+	public String toString() {
+
+	}
+	// No deleteQuiz or addQuiz methods 
+	// addQuiz should just use a constructor and write method
+	// If course class includes arraylist of quizzes, deleteQuiz should just remove one from list 
+}
