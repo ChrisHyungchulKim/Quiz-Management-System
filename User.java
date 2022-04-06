723d8b891f3ac3446e7041359da97f045f857867
Raohk <khoa.raisr@gmail.com>
2022-04-06 14:05:42 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,6 +1,8 @@
 public interface User {
-    public String getName();        //returns name
-    public void setName(String name);   // sets name
+    public String getFirstName();        //returns first name
+    public void setFirstName(String firstName);   // sets first name
+    public String getLastName();        //returns last name
+    public void setLastName(String lastName);   // sets last name
     public String getPassword();    //returns password
     public void setPassword(String password);   //sets a new password
     public int getUserID();     //returns the UserID
