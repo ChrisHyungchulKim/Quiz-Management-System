830aee0035f88bf9a94742db6953f16ef234f3ad
Raohk <khoa.raisr@gmail.com>
2022-04-06 13:53:23 -0400

    Add files via upload

---
+++ User.java
@@ -0,0 +1,8 @@
+public interface User {
+    public String getName();        //returns name
+    public void setName(String name);   // sets name
+    public String getPassword();    //returns password
+    public void setPassword(String password);   //sets a new password
+    public int getUserID();     //returns the UserID
+    public void setUserID(int userID);  //sets UserID
+}
