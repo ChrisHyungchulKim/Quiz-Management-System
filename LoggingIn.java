b78fa5410c2012f0a694234bafe397b747b343eb
ctuins <47837726+ctuins@users.noreply.github.com>
2022-04-08 17:14:34 -0400

    Update LoggingIn.java

--- LoggingIn.java
+++ LoggingIn.java
@@ -5,66 +5,7 @@ import java.util.Scanner;
 
 public class LoggingIn {
 
-    public static final String CREATE_ACCOUNT_QUESTION = "1. Log In\n2.Create Account\n";
-    public static final String TEACHER_QUESTION = "Are you a teacher?\n";
-    
-    //Sample Main Method
-    /**public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        boolean ifContinue = true;
-
-        System.out.println("Welcome to the Quiz Section!");
-
-        int accountQuestion;
-        do {
-            System.out.print(CREATE_ACCOUNT_QUESTION);
-            accountQuestion = scanner.nextInt();
-            scanner.nextLine();
-            if (accountQuestion != 1 && accountQuestion != 2) {
-                System.out.println("Please enter either 1 or 2.");
-            }
-        } while (accountQuestion != 1 && accountQuestion != 2);
-
-        while (ifContinue) {
-            System.out.println("Please enter your username: ");
-            String username = scanner.nextLine();
-
-            System.out.println("Please enter your password: ");
-            String password = scanner.nextLine();
-
-            String teacher;
-            do {
-                System.out.print(TEACHER_QUESTION);
-                teacher = scanner.nextLine();
-                if (!Objects.equals(teacher.toLowerCase(), "yes") && !Objects.equals(teacher.toLowerCase(), "no")) {
-                    System.out.println("Please enter either yes or no.");
-                }
-            } while (!Objects.equals(teacher.toLowerCase(), "yes") && !Objects.equals(teacher.toLowerCase(), "no"));
-
-            boolean ifTeacher;
-            ifTeacher = teacher.equalsIgnoreCase("yes");
-
-            if (accountQuestion == 2) {
-                createUser(username, password, ifTeacher);
-                ifContinue = false;
-            } else {
-                boolean checkUser = !checkUser(username, password, ifTeacher);
-                if (checkUser) {
-                    System.out.println("Your login information isn't correct! Please try again.");
-                } else {
-                    ifContinue = false;
-                }
-            }
-        }
-
-
-
-    }*/
-
-
-    public static boolean checkUser(String username, String password, boolean teacher) {
+    public static boolean checkUser(User user) {
         try {
             boolean hasCorrectInfo = false;
 
@@ -79,9 +20,9 @@ public class LoggingIn {
 
             //go through the userDetailFile and check for the line with the username that you want to edit
             for (int i = 0; i < userDetailFile.size(); i++) {
-                if (userDetailFile.get(i).contains(username)) {
-                    if (userDetailFile.get(i + 1).contains(password)) {
-                        if (userDetailFile.get(i + 2).contains(String.valueOf(teacher))) {
+                if (userDetailFile.get(i).contains(user.getUsername())) {
+                    if (userDetailFile.get(i + 1).contains(user.getPassword())) {
+                        if (userDetailFile.get(i + 2).contains(String.valueOf(user.isTeacher()))) {
                             hasCorrectInfo = true;
                         }
                     }
@@ -93,7 +34,7 @@ public class LoggingIn {
             return false;
         }
     }
-    public static boolean editPassword(String username, String oldPassword, String newPassword) {
+    public static boolean editPassword(User user, String oldPassword, String newPassword) {
         try {
             boolean worked = false;
 
@@ -108,7 +49,7 @@ public class LoggingIn {
 
             //go through the userDetailFile and check for the line with the username that you want to edit
             for (int i = 0; i < userDetailFile.size(); i++) {
-                if (userDetailFile.get(i).contains(username)) {
+                if (userDetailFile.get(i).contains(user.getUsername())) {
                     String[] editUser = userDetailFile.get(i + 1).split(" ");
                     for (int f = 0; f < editUser.length; f++) {
                         if (editUser[f].contains(oldPassword)) {
@@ -137,7 +78,7 @@ public class LoggingIn {
         }
     }
 
-    public static boolean editUsername(String username, String newUsername) {
+    public static boolean editUsername(User user, String newUsername) {
         try {
             boolean worked = false;
 
@@ -145,7 +86,6 @@ public class LoggingIn {
             BufferedReader reader = new BufferedReader(new FileReader("UserDetails.txt"));
             ArrayList<String> userDetailFile = new ArrayList<>();
             String line = reader.readLine();
-            System.out.println(line);
             while (line != null) {
                 userDetailFile.add(line);
                 line = reader.readLine();
@@ -153,10 +93,10 @@ public class LoggingIn {
 
             //go through the userDetailFile and check for the line with the username that you want to edit
             for (int i = 0; i < userDetailFile.size(); i++) {
-                if (userDetailFile.get(i).contains(username)) {
+                if (userDetailFile.get(i).contains(user.getUsername())) {
                     String[] editUser = userDetailFile.get(i).split(" ");
                     for (int f = 0; f < editUser.length; f++) {
-                        if (editUser[f].contains(username)) {
+                        if (editUser[f].contains(user.getUsername())) {
                             editUser[f] = " " + newUsername;
                         }
                     }
@@ -180,21 +120,59 @@ public class LoggingIn {
         }
     }
 
-    public static boolean createUser(String username, String password, boolean teacher) {
+    public static boolean createUser(User user) {
         try {
             //establish FileWriter to the UserDetails.txt text file
             FileWriter writer = new FileWriter("UserDetails.txt", true);
-            boolean contains = checkRedundant(username);
+            boolean contains = checkRedundant(user.getUsername());
+            System.out.println(contains);
 
             if (contains) {
                 return false;
             } else {
                 String userDetails = String.format("Username: %s\nPassword: %s\nTeacher: %s\n",
-                                                    username, password, teacher);
+                                                    user.getUsername(), user.getPassword(), user.isTeacher());
                 writer.write(userDetails);
                 writer.close();
                 return true;
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean deleteAccount(User user)  {
+        try {
+            boolean worked = false;
+
+            //read the file and put it in an Array list
+            BufferedReader reader = new BufferedReader(new FileReader("UserDetails.txt"));
+            ArrayList<String> userDetailFile = new ArrayList<>();
+            String line = reader.readLine();
+            while (line != null) {
+                userDetailFile.add(line);
+                line = reader.readLine();
+            }
+
+            //go through the userDetailFile and check for the line with the username that you want to edit
+            for (int i = 0; i < userDetailFile.size(); i++) {
+                if (userDetailFile.get(i).contains(user.getUsername())) {
+                    if (userDetailFile.get(i + 1).contains(user.getPassword())) {
+                        userDetailFile.set(i, "01100100 01100101 01101100 01100101 01110100 01100101 01100100");
+                        userDetailFile.set(i + 1, "01100100 01100101 01101100 01100101 01110100 01100101 01100100");
+                        userDetailFile.set(i + 2, "01100100 01100101 01101100 01100101 01110100 01100101 01100100");
+                        worked = true;
+                    }
+                }
+            }
+            FileWriter writer = new FileWriter("UserDetails.txt");
+            for(int i = 0; i < userDetailFile.size(); i++) {
+                writer.write(userDetailFile.get(i) + "\n");
+            }
+            writer.close();
+            return worked;
+
         } catch (IOException e) {
             e.printStackTrace();
             return false;
