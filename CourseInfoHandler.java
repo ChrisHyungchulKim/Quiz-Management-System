2a7334fcca4c34c8bae81d8da127195a311a6a84
RKOKA209 <48864636+RKOKA209@users.noreply.github.com>
2022-04-09 14:35:13 -0400

    Update CourseInfoHandler.java

--- CourseInfoHandler.java
+++ CourseInfoHandler.java
@@ -17,13 +17,41 @@ public class CourseInfoHandler {
         this.courseList = courseList;
     }
 
-    public void writeCourseInfo () {
+    public void deleteCourse(Course deleteCourse) {
+        for(int i = 0; i < this.courseList.size(); i++) {
+            if (courseList.get(i).getCourseName().equals(deleteCourse.getCourseName()) &&
+                    courseList.get(i).getOwner().equals(deleteCourse.getOwner())) {
+                this.courseList.remove(this.courseList.get(i));
+            }
+        }    }
+
+    public void editCourseList(Course editedCourse) {
+        for(int i = 0; i < this.courseList.size(); i++) {
+            if (courseList.get(i).getCourseName().equals(editedCourse.getCourseName()) &&
+                    courseList.get(i).getOwner().equals(editedCourse.getOwner())) {
+                this.courseList.remove(courseList.get(i));
+                this.courseList.add(i, editedCourse);
+            }
+        }
+    }
+
+    public boolean checkIfDuplicate(Course course) {
+        for (int i = 0; i < this.courseList.size(); i++) {
+            if (courseList.get(i).getCourseName().equals(course.getCourseName()) &&
+                    courseList.get(i).getOwner().equals(course.getOwner())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void writeCourseInfo() {
 
         String courseDetails = "";
         int index = 0;
 
         try {
-            File courseInfoFile = new File("CourseDetails");
+            File courseInfoFile = new File("CourseDetails.txt");
             FileWriter fileWriter = new FileWriter(courseInfoFile);
             PrintWriter printWriter = new PrintWriter(fileWriter);
 
@@ -34,15 +62,15 @@ public class CourseInfoHandler {
                     courseDetails += "\nCourse: " + c.getCourseName();
                 }
                     //TODO:PRINT OWNER INFO
-                    //courseDetails += "\nOwner" + c.get();
+                    courseDetails += "\nOwner_Username: " + c.getOwner().getUsername();
                 for (Quiz q : c.getQuizzes()) {
-                    courseDetails += "\nQuiz Name: " + q.getName();
+                    courseDetails += "\nQuiz_Name: " + q.getName();
                     for (Question question : q.getQuestions()) {
                         courseDetails += "\nQuestion: " + question.getPrompt();
                         courseDetails += "\nWeight: " + question.getWeight();
                         for (int i = 0; i < question.getResponses().size(); i++){
                             if(i == question.getAnswer()) {
-                                courseDetails += "\nCorrect Answer: " + question.getResponses().get(i);
+                                courseDetails += "\nCorrect_Answer: " + question.getResponses().get(i);
                             } else {
                                 courseDetails += "\nAnswer: " + question.getResponses().get(i);
                             }
@@ -60,37 +88,76 @@ public class CourseInfoHandler {
         }
     }
 
-    public void ReadCourseInfo () {
+    public void ReadCourseInfo() {
 
         String line;
         String courseName;
-        String Owner;
+        String owner;
+        String quizName;
+        String prompt;
+        int weight;
+        int answer;
+        ArrayList<Question> questionList;
+        ArrayList<String> answerChoices;
+        ArrayList<Quiz> quizList;
 
         int courseNumber;
 
         try {
-            File courseInfoFile = new File("CourseDetails");
+            File courseInfoFile = new File("CourseDetails.txt");
             FileReader fileReader = new FileReader(courseInfoFile);
             BufferedReader bfr = new BufferedReader(fileReader);
 
+            weight = 0;
+            answer = 0;
             courseNumber = 0;
             line = bfr.readLine();
 
             while (line != null) {
+
                 if(line.contains("Course: ")) {
-                    //courseList.add(new Course(courseName, ,null));
+                    quizList = new ArrayList<Quiz>();
+                    courseName = line.substring(line.indexOf(' ') + 1);
+                    line = bfr.readLine();
+                    if(line.contains("Owner_Username: ")) {
+                        owner = line.substring(line.indexOf(' '));
+                    }
+                    line = bfr.readLine();
+                    while(line.contains("Quiz_Name: ")) {
+                        questionList = new ArrayList<Question>();
+                        quizName = line.substring(line.indexOf(' '));
+                        line= bfr.readLine();
+                        while(line.contains("Question: ")) {
+                            answerChoices = new ArrayList<String>();
+                            prompt = line.substring(line.indexOf(' '));
+                            line = bfr.readLine();
+                            if (line.contains("Weight: ")) {
+                                weight = Integer.parseInt(line.substring(line.indexOf(' ')));
+                            }
+                            line = bfr.readLine();
+                            while(line.contains("Answer: ")) {
+                                if (line.contains("Correct_Answer: ")) {
+                                    answerChoices.add(line.substring(line.indexOf(' ')));
+                                    answer = answerChoices.size() - 1;
+                                } else {
+                                    answerChoices.add(line.substring(line.indexOf(' ')));
+                                }
+                                line = bfr.readLine();
+                            }
+                            questionList.add(new Question(prompt, answerChoices, answer, weight));
+                        }
+                        quizList.add(new Quiz(quizName, questionList));
+                    }
+                    //TODO:Once a method is added for the returns a user given a username
+                    //courseList.add(new Course(courseName,,quizList));
                 }
 
                 courseNumber++;
             }
 
-
-
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-
-
     }
 
 }
