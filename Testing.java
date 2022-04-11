65e3885af93f7638912b6b1dfbd5020ce4138304
Raohk <khoa.raisr@gmail.com>
2022-04-11 15:00:59 -0400

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -53,34 +53,33 @@ public class Testing {
 
 //     }
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
+
+        //for User class
+        String username = "";
+        String password = "";
+        boolean teacher = true;
+        User user = new User(username, password, teacher);
+
+        user.setUsername("Teacher");
+        user.getUsername();
+        user.setPassword("1122");
+        user.getPassword();
+        user.setTeacher(true);
+        user.isTeacher();
+
+        if (!user.getUsername().equals("Teacher")) {
+            System.out.println("Not the expected output");
+        }
+        if (!user.getPassword().equals("1122")) {
+            System.out.println("Not the expected output");
+        }
+        if (!user.isTeacher() == true) {
+            System.out.println("Not the expected output");
+        }
 
 
-       //for User class
-       String username = "";
-       String password = "";
-       boolean teacher = true;
-       User user = new User(username, password, teacher);
-
-       user.setUsername("Teacher");
-       user.getUsername();
-       user.setPassword("1122");
-       user.getPassword();
-       user.setTeacher(true);
-       user.isTeacher();
-
-       if (!user.getUsername().equals("Teacher")) {
-           System.out.println("Not the expected output");
-       }
-       if (!user.getPassword().equals("1122")) {
-           System.out.println("Not the expected output");
-       }
-       if (!user.isTeacher() == true) {
-           System.out.println("Not the expected output");
-       }
-  
-    
-    //for Submission class
+        //for Submission class
 
         User student = null;
         ArrayList<String> responses = new ArrayList<>();
@@ -126,21 +125,79 @@ public class Testing {
         submission.isGraded();
         System.out.println(submission.isGraded());
 
+        //for LoggingIn
+        boolean allGood = true;
+        User teacherLoggingIn = new User("James", "jPassword", true);
+        User studentLoggingIn = new User("John", "JohnsPassword", false);
+        User notRegisteredTeacher = new User("Ronnie", "1234", true);
+        User notRegisteredStudent = new User("lucy", "xyz", false);
 
+        //Testing createUser method
+        LoggingIn.createUser(teacherLoggingIn);
+        LoggingIn.createUser(student);
+        if (!LoggingIn.checkUser(teacherLoggingIn)) {
+            System.out.println("[checkUser] Not the expected output teacher");
+            allGood = false;
+        }
+        if (!LoggingIn.checkUser(student)) {
+            System.out.println("[checkUser] Not the expected output student");
+            allGood = false;
+        }
+        //Checking login class for users that are not registered
+        if (LoggingIn.checkUser(notRegisteredTeacher)) {
+            System.out.println("[checkUser] Not the expected output teacher");
+            allGood = false;
+        }
+        if (LoggingIn.checkUser(notRegisteredStudent)) {
+            System.out.println("[checkUser] Not the expected output for student");
+            allGood = false;
+        }
+        //Testing editPassword
+        LoggingIn.editPassword(teacherLoggingIn, "jPassword", "newPassword");
+        LoggingIn.editPassword(studentLoggingIn, "JohnsPassword", "newPassword2");
+        if (!teacherLoggingIn.getPassword().equals("newPassword")) {
+            System.out.println("[editPassword] Not the expected output");
+            allGood = false;
+        }
+        if (!studentLoggingIn.getPassword().equals("newPassword2")) {
+            System.out.println("[editPassword] Not the expected output");
+            allGood = false;
+        }
 
+        //Testing editUsername
+        LoggingIn.editUsername(teacherLoggingIn, "Kasper");
+        LoggingIn.editUsername(studentLoggingIn, "Jasper");
+        if (!teacherLoggingIn.getUsername().equals("Kasper")) {
+            System.out.println("[editUsername] Not the expected output");
+            allGood = false;
+        }
+        if (!studentLoggingIn.getUsername().equals("Jasper")) {
+            System.out.println("[editUsername] Not the expected output");
+            allGood = false;
+        }
+
+        //Testing checkRedundant
+        if (!LoggingIn.checkRedundant("Kasper")) {
+            System.out.println("[checkRedundant] Not the expected output");
+            allGood = false;
+        }
+        if (!LoggingIn.checkRedundant("Jasper")) {
+            System.out.println("[checkRedundant] Not the expected output");
+            allGood = false;
+        }
+
+        //Testing getUser
+        if (!LoggingIn.getUser("Kasper").equals(teacher)) {
+            System.out.println("[getUser] Not the expected output");
+            allGood = false;
+        }
+        if (!LoggingIn.getUser("Jasper").equals(student)) {
+            System.out.println("[getUser] Not the expected output");
+            allGood = false;
+        }
+
+        if (allGood) {
+            System.out.println("All test cases passed!");
+        }
+    }
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
