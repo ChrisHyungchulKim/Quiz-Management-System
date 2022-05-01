e12cafc8d47c84456feac85afe6d88100c3f9c64
RKOKA209 <48864636+RKOKA209@users.noreply.github.com>
2022-04-30 22:10:12 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -2,15 +2,18 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Server {
 
     private static Class info;
+    private static ArrayList<Submission> submissions;
     private static Object concur;
 
     public static void main(String[] args) {
 
         info = new Class(CourseInfoHandler.readCourseInfo());
+        submissions = readSubmissions(info);
         concur = new Object();
 
         try {
@@ -31,7 +34,6 @@ public class Server {
                     PrintWriter writer = new PrintWriter(socket.getOutputStream());
                     String message = null;
                     while (message == null) {
-                        //reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                         message = reader.readLine();
                     }
 
@@ -43,10 +45,11 @@ public class Server {
                     } else if (message.equalsIgnoreCase("Update Arraylist")) {
                         synchronized (concur) {
                             sendArrayList(socket);
-                            System.out.println("test 1");
                             receiveArrayList(socket);
-                            System.out.println("test 2");
+                            System.out.printf(info.getCourses().get(0).getCourseName());
                         }
+                    } else if (message.equalsIgnoreCase("Get Submissions Arraylist")){
+                        sendArrayList(socket);
                     }
 
                 }
@@ -60,6 +63,20 @@ public class Server {
 
     }
 
+
+    public void writeSubmissions(Socket socket, Submission submission) {
+
+    }
+
+    public void sendSubmissions(Socket socket) {
+        try {
+            ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());
+            objectOutput.writeObject(readSubmissions(info));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
     public static void updateCourse(ArrayList<Course> courses) {
         synchronized (concur) {
             info.setCourses(courses);
@@ -67,26 +84,18 @@ public class Server {
     }
 
     /**
-     *
-     *
      * @param socket - The socket the that the server communication is started on
      */
     public static void sendArrayList(Socket socket) {
         try {
             ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());
-            objectOutput.writeObject(info.getCourses());
-            objectOutput.flush();
-            for (Course s: info.getCourses()) {
-                System.out.println(s.getCourseName());
-            }
-            System.out.println("");
+            objectOutput.writeObject(getInfoCourseArray());
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
     }
 
     /**
-     *
      * @param socket - the socket the that the server communication is started on
      */
     public static void receiveArrayList(Socket socket) {
@@ -95,11 +104,6 @@ public class Server {
             Object object = objectInput.readObject();
             setInfoCourseArray((ArrayList<Course>) object);
             info.setCourses((ArrayList<Course>) object);
-            for (Course s: (ArrayList<Course>) object) {
-                System.out.println(s.getCourseName());
-            }
-            System.out.println("");
-
         } catch (IOException ioe) {
             ioe.printStackTrace();
         } catch (ClassNotFoundException e) {
@@ -107,7 +111,8 @@ public class Server {
         }
     }
 
-    /**Getter for the Courses Arraylist
+    /**
+     * Getter for the Courses Arraylist
      *
      * @return - Courses Arraylist
      */
@@ -115,7 +120,8 @@ public class Server {
         return info.getCourses();
     }
 
-    /**Sets the info objects courses arraylist field to the new one provided by the parameter
+    /**
+     * Sets the info objects courses arraylist field to the new one provided by the parameter
      *
      * @param courses - new version of the courses Arraylist
      */
@@ -124,8 +130,101 @@ public class Server {
         CourseInfoHandler.writeCourseInfo(info);
     }
 
+    public static ArrayList<Submission> readSubmissions(Class currentClass) {
+        BufferedReader reader;
+        try {
+            FileReader filereader = new FileReader("SubmissionDetails.txt");
+            reader = new BufferedReader(filereader);
+            String line = reader.readLine();
+            ArrayList<User> users = LoggingIn.readUserInfo();
 
 
+            String studentName = "";
+            String courseName = "";
+            Course courseUsed = null;
+            Quiz currentQuiz = null;
+            User student = null;
+            ArrayList<String> responses;
+            ArrayList<String> grades = null;
+            String time = "";
+            String quizBeingTaken = "";
+            boolean graded = false;
+            double grade = 0;
+            String currentQuestion = null;
+            Question theQuestion = null;
+            Submission newSubmission = null;
+            ArrayList<Submission> submissions = new ArrayList<>();
+            while (line != null) {
+                if (line.contains("Student: ")) {
+                    responses = new ArrayList<String>();
+                    studentName = line.substring(line.indexOf(' ') + 1);
+                    for (int i = 0; i < users.size(); i++) {
+                        if (users.get(i).getUsername().equals(studentName)) {
+                            student = users.get(i);
+                        }
+                    }
+                    line = reader.readLine();
 
-}
+                    if (line.contains("Responses: ")) {
+                        String[] responsesArray = line.split(",");
+                        responses = new ArrayList<String>(Arrays.asList(responsesArray));
+                        line = reader.readLine();
+                        if (line.contains("Course: ")) {
+                            courseName = line.substring(line.indexOf(' ') + 1);
+                            for (int i = 0; i < currentClass.getCourses().size(); i++) {
+                                if (currentClass.getCourses().get(i).getCourseName().equals(courseName)) {
+                                    courseUsed = currentClass.getCourses().get(i);
+                                }
+                            }
+                            line = reader.readLine();
+
+                            if (line.contains("Quiz: ")) {
+                                quizBeingTaken = line.substring(line.indexOf(' ') + 1);
+                                for (int i = 0; i < courseUsed.getQuizzes().size(); i++) {
+                                    if (courseUsed.getQuizzes().get(i).getName().equals(quizBeingTaken)) {
+                                        currentQuiz = courseUsed.getQuizzes().get(i);
+                                    }
+                                }
+                                line = reader.readLine();
+
+                                if (line.contains("Time: ")) {
+                                    time = line.substring(line.indexOf(' ') + 1);
+                                    line = reader.readLine();
+                                    if (line.contains("Graded: ")) {
+                                        graded = Boolean.parseBoolean(line.substring(line.indexOf(' ') + 1));
+                                        line = reader.readLine();
+
+                                        if (line.contains("Grades:")) {
+                                            if (line.contains(",")) {
+                                                String gradeSplit = line.substring(line.indexOf(' ') + 1);
+                                                String[] gradesArray = gradeSplit.split(",");
+                                                grades = new ArrayList<String>(Arrays.asList(responsesArray));
+                                            } else {
+                                                grades = new ArrayList<>();
+                                            }
+
+                                            line = reader.readLine();
+                                        }
+
+                                    }
+                                }
+
+
+                            }
+                        }
+                    }
+                    newSubmission = new Submission(student, currentQuiz, courseUsed,
+                            responses, time, graded, grades);
+                    submissions.add(newSubmission);
+                    //line = reader.readLine();
+                }
+
+            }
+            reader.close();
+            return submissions;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
 
